c3c752c5694f20dc112e3fa799db54e226f19e79
Stephen Francis <stephen.francis@rullion.co.uk>
2017-04-19 08:54:03 +0100

    fix test

--- test.js
+++ test.js
@@ -10,7 +10,7 @@ var last_end = 0;
 module.exports.Base_clone = function (test) {
     test.expect(9);
 
-    test.ok(Core.Base.id === "Base", "Base.id === 'Base'");
+    test.equals(Core.Base.id, "Base", "Base.id === 'Base'");
     test.throws(function () { Core.Base.clone(); });
     test.throws(function () { Core.Base.clone({}); });
     test.throws(function () { Core.Base.clone({ name: "blah", }); });
@@ -48,17 +48,17 @@ module.exports.Base_rest = function (test) {
         return a.speak.call(this) + ", et Bonjour";
     });
 
-    test.ok(a.id === "a", "a.id === 'a'");
-    test.ok(a.toString() === "/Base/a", "a.toString() === '/Base/a'");
-    // test.ok(a.path() === "a", "a.path()     ===  'a'");
-    // test.ok(a.descent() === "/Base/a", "a.descent()  === '/Base/a'");
-    test.ok(a.getName() === "Dicky", "a.getName() works");
-    test.ok(a.speak() === "Hello Dicky", "a remains unchanged");
-    test.ok(b.toString() === "/Base/a/b", "b.toString() === '/Base/a/b'");
-    // test.ok(b.path() === "b", "b.path()     ===    'b'");
-    // test.ok(b.descent() === "/Base/a/b", "b.descent()  === '/Base/a/b'");
-    test.ok(b.getName() === "Suzie", "b.getName() works");
-    test.ok(b.speak() === "Hello Suzie, et Bonjour", "b inherits a functionality");
+    test.equals(a.id, "a", "a.id === 'a'");
+    test.equals(a.toString(), "/Base/a", "a.toString() === '/Base/a'");
+    // test.equals(a.path(), "a", "a.path()     ===  'a'");
+    // test.equals(a.descent(), "/Base/a", "a.descent()  === '/Base/a'");
+    test.equals(a.getName(), "Dicky", "a.getName() works");
+    test.equals(a.speak(), "Hello Dicky", "a remains unchanged");
+    test.equals(b.toString(), "/Base/a/b", "b.toString() === '/Base/a/b'");
+    // test.equals(b.path(), "b", "b.path()     ===    'b'");
+    // test.equals(b.descent(), "/Base/a/b", "b.descent()  === '/Base/a/b'");
+    test.equals(b.getName(), "Suzie", "b.getName() works");
+    test.equals(b.speak(), "Hello Suzie, et Bonjour", "b inherits a functionality");
 
     try {
         a.define("sex", "F");
@@ -227,6 +227,7 @@ module.exports.Log_fatal = function (test) {
         id: "a",
         name: "Dicky",
         type: "Person",
+        log_show_caller: false,
     });
     var last_log_msg;
     test.expect(50);
@@ -237,52 +238,52 @@ module.exports.Log_fatal = function (test) {
     });
 
     a.setLogLevel(a.log_levels.trace);
-    a.fatal("a"); test.ok(last_log_msg === "FATAL: a", "trace / fatal");
-    a.error("b"); test.ok(last_log_msg === "ERROR: b", "trace / error");
-    a.warn("c"); test.ok(last_log_msg === "WARN : c", "trace / warn");
-    a.info("d"); test.ok(last_log_msg === "INFO : d", "trace / info");
-    a.debug("e"); test.ok(last_log_msg === "DEBUG: e", "trace / debug");
-    a.trace("f"); test.ok(last_log_msg === "TRACE: f", "trace / trace");
+    a.fatal("a"); test.equals(last_log_msg, "FATAL: a", "trace / fatal");
+    a.error("b"); test.equals(last_log_msg, "ERROR: b", "trace / error");
+    a.warn("c"); test.equals(last_log_msg, "WARN : c", "trace / warn");
+    a.info("d"); test.equals(last_log_msg, "INFO : d", "trace / info");
+    a.debug("e"); test.equals(last_log_msg, "DEBUG: e", "trace / debug");
+    a.trace("f"); test.equals(last_log_msg, "TRACE: f", "trace / trace");
 
     a.setLogLevel(a.log_levels.debug);
-    a.fatal("a"); test.ok(last_log_msg === "FATAL: a", "debug / fatal");
-    a.error("b"); test.ok(last_log_msg === "ERROR: b", "debug / error");
-    a.warn("c"); test.ok(last_log_msg === "WARN : c", "debug / warn");
-    a.info("d"); test.ok(last_log_msg === "INFO : d", "debug / info");
-    a.debug("e"); test.ok(last_log_msg === "DEBUG: e", "debug / debug");
-    a.trace("f"); test.ok(last_log_msg === "DEBUG: e", "debug / trace");
+    a.fatal("a"); test.equals(last_log_msg, "FATAL: a", "debug / fatal");
+    a.error("b"); test.equals(last_log_msg, "ERROR: b", "debug / error");
+    a.warn("c"); test.equals(last_log_msg, "WARN : c", "debug / warn");
+    a.info("d"); test.equals(last_log_msg, "INFO : d", "debug / info");
+    a.debug("e"); test.equals(last_log_msg, "DEBUG: e", "debug / debug");
+    a.trace("f"); test.equals(last_log_msg, "DEBUG: e", "debug / trace");
 
     a.setLogLevel(a.log_levels.info);
-    a.fatal("a"); test.ok(last_log_msg === "FATAL: a", "info  / fatal");
-    a.error("b"); test.ok(last_log_msg === "ERROR: b", "info  / error");
-    a.warn("c"); test.ok(last_log_msg === "WARN : c", "info  / warn");
-    a.info("d"); test.ok(last_log_msg === "INFO : d", "info  / info");
-    a.debug("e"); test.ok(last_log_msg === "INFO : d", "info  / debug");
-    a.trace("f"); test.ok(last_log_msg === "INFO : d", "info  / trace");
+    a.fatal("a"); test.equals(last_log_msg, "FATAL: a", "info  / fatal");
+    a.error("b"); test.equals(last_log_msg, "ERROR: b", "info  / error");
+    a.warn("c"); test.equals(last_log_msg, "WARN : c", "info  / warn");
+    a.info("d"); test.equals(last_log_msg, "INFO : d", "info  / info");
+    a.debug("e"); test.equals(last_log_msg, "INFO : d", "info  / debug");
+    a.trace("f"); test.equals(last_log_msg, "INFO : d", "info  / trace");
 
     a.setLogLevel(a.log_levels.warn);
-    a.fatal("a"); test.ok(last_log_msg === "FATAL: a", "warn  / fatal");
-    a.error("b"); test.ok(last_log_msg === "ERROR: b", "warn  / error");
-    a.warn("c"); test.ok(last_log_msg === "WARN : c", "warn  / warn");
-    a.info("d"); test.ok(last_log_msg === "WARN : c", "warn  / info");
-    a.debug("e"); test.ok(last_log_msg === "WARN : c", "warn  / debug");
-    a.trace("f"); test.ok(last_log_msg === "WARN : c", "warn  / trace");
+    a.fatal("a"); test.equals(last_log_msg, "FATAL: a", "warn  / fatal");
+    a.error("b"); test.equals(last_log_msg, "ERROR: b", "warn  / error");
+    a.warn("c"); test.equals(last_log_msg, "WARN : c", "warn  / warn");
+    a.info("d"); test.equals(last_log_msg, "WARN : c", "warn  / info");
+    a.debug("e"); test.equals(last_log_msg, "WARN : c", "warn  / debug");
+    a.trace("f"); test.equals(last_log_msg, "WARN : c", "warn  / trace");
 
     a.setLogLevel(a.log_levels.error);
-    a.fatal("a"); test.ok(last_log_msg === "FATAL: a", "error / fatal");
-    a.error("b"); test.ok(last_log_msg === "ERROR: b", "error / error");
-    a.warn("c"); test.ok(last_log_msg === "ERROR: b", "error / warn");
-    a.info("d"); test.ok(last_log_msg === "ERROR: b", "error / info");
-    a.debug("e"); test.ok(last_log_msg === "ERROR: b", "error / debug");
-    a.trace("f"); test.ok(last_log_msg === "ERROR: b", "error / trace");
+    a.fatal("a"); test.equals(last_log_msg, "FATAL: a", "error / fatal");
+    a.error("b"); test.equals(last_log_msg, "ERROR: b", "error / error");
+    a.warn("c"); test.equals(last_log_msg, "ERROR: b", "error / warn");
+    a.info("d"); test.equals(last_log_msg, "ERROR: b", "error / info");
+    a.debug("e"); test.equals(last_log_msg, "ERROR: b", "error / debug");
+    a.trace("f"); test.equals(last_log_msg, "ERROR: b", "error / trace");
 
     a.setLogLevel(a.log_levels.fatal);
-    a.fatal("a"); test.ok(last_log_msg === "FATAL: a", "fatal / fatal");
-    a.error("b"); test.ok(last_log_msg === "FATAL: a", "fatal / error");
-    a.warn("c"); test.ok(last_log_msg === "FATAL: a", "fatal / warn");
-    a.info("d"); test.ok(last_log_msg === "FATAL: a", "fatal / info");
-    a.debug("e"); test.ok(last_log_msg === "FATAL: a", "fatal / debug");
-    a.trace("f"); test.ok(last_log_msg === "FATAL: a", "fatal / trace");
+    a.fatal("a"); test.equals(last_log_msg, "FATAL: a", "fatal / fatal");
+    a.error("b"); test.equals(last_log_msg, "FATAL: a", "fatal / error");
+    a.warn("c"); test.equals(last_log_msg, "FATAL: a", "fatal / warn");
+    a.info("d"); test.equals(last_log_msg, "FATAL: a", "fatal / info");
+    a.debug("e"); test.equals(last_log_msg, "FATAL: a", "fatal / debug");
+    a.trace("f"); test.equals(last_log_msg, "FATAL: a", "fatal / trace");
 
     a.fatal("a");
     a.fatal("a");
@@ -304,27 +305,27 @@ module.exports.Log_fatal = function (test) {
 
     a.debug("e");
 
-    test.ok(a.log_counters[a.log_levels.fatal] === 11, "11 fatals");
-    test.ok(a.log_counters[a.log_levels.error] === 10, "10 errors");
-    test.ok(a.log_counters[a.log_levels.warn] === 9, "9 warns");
-    test.ok(a.log_counters[a.log_levels.info] === 8, "8 infos");
-    test.ok(a.log_counters[a.log_levels.debug] === 7, "7 debugs");
-    test.ok(a.log_counters[a.log_levels.trace] === 6, "6 traces");
+    test.equals(a.log_counters[a.log_levels.fatal], 11, "11 fatals");
+    test.equals(a.log_counters[a.log_levels.error], 10, "10 errors");
+    test.equals(a.log_counters[a.log_levels.warn], 9, "9 warns");
+    test.equals(a.log_counters[a.log_levels.info], 8, "8 infos");
+    test.equals(a.log_counters[a.log_levels.debug], 7, "7 debugs");
+    test.equals(a.log_counters[a.log_levels.trace], 6, "6 traces");
 
     a.printLogCounters();
-    test.ok(last_log_msg === "TRACE: 6, DEBUG: 7, INFO : 8, WARN : 9, ERROR: 10, FATAL: 11", "report 1: " + last_log_msg);
+    test.equals(last_log_msg, "TRACE: 6, DEBUG: 7, INFO : 8, WARN : 9, ERROR: 10, FATAL: 11", "report 1: " + last_log_msg);
 
     a.resetLogCounters();
 
-    test.ok(a.log_counters[a.log_levels.fatal] === 0, "0 fatals");
-    test.ok(a.log_counters[a.log_levels.error] === 0, "0 errors");
-    test.ok(a.log_counters[a.log_levels.warn] === 0, "0 warns");
-    test.ok(a.log_counters[a.log_levels.info] === 0, "0 infos");
-    test.ok(a.log_counters[a.log_levels.debug] === 0, "0 debugs");
-    test.ok(a.log_counters[a.log_levels.trace] === 0, "0 traces");
+    test.equals(a.log_counters[a.log_levels.fatal], 0, "0 fatals");
+    test.equals(a.log_counters[a.log_levels.error], 0, "0 errors");
+    test.equals(a.log_counters[a.log_levels.warn], 0, "0 warns");
+    test.equals(a.log_counters[a.log_levels.info], 0, "0 infos");
+    test.equals(a.log_counters[a.log_levels.debug], 0, "0 debugs");
+    test.equals(a.log_counters[a.log_levels.trace], 0, "0 traces");
 
     a.printLogCounters();
-    test.ok(last_log_msg === "TRACE: 0, DEBUG: 0, INFO : 0, WARN : 0, ERROR: 0, FATAL: 0", "report 2: " + last_log_msg);
+    test.equals(last_log_msg, "TRACE: 0, DEBUG: 0, INFO : 0, WARN : 0, ERROR: 0, FATAL: 0", "report 2: " + last_log_msg);
 
     test.done();
 };
